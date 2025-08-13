57296a0539fda20e1ddc2f41804c406f3188e72a
mjfy93 <maria.jose.ferro@hotmail.com>
2025-08-13 17:38:30 -0500

    firstdraft

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import "bootstrap";
+ "bootstrap";
 import "./style.css";
 
 
@@ -7,5 +7,12 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  let pronouns =['the', 'my', 'his', 'a', 'we'];
+  let adjectives = ['happy', 'dark', 'bright', 'sleepy', 'great'];
+  let nouns =['cat', 'cactus', 'hummingbird', 'tulip', 'faerie'];
+  let extensions = ['.com', '.net', '.co', '.io', '.gov'];
+  let randomPronoun = Math.random(Math.floor()) 
+
+
   console.log("Hello Rigo from the console!");
 };
