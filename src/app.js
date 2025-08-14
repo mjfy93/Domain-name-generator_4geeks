c1984ca669276dcae1c58d7f1262fe3becd58268
mjfy93 <maria.jose.ferro@hotmail.com>
2025-08-14 15:57:10 -0500

    Domain Name Generator Done

--- src/app.js
+++ src/app.js
@@ -1,18 +1,39 @@
- "bootstrap";
+"bootstrap";
 import "./style.css";
 
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  let pronouns =['the', 'my', 'his', 'a', 'we'];
-  let adjectives = ['happy', 'dark', 'bright', 'sleepy', 'great'];
-  let nouns =['cat', 'cactus', 'hummingbird', 'tulip', 'faerie'];
-  let extensions = ['.com', '.net', '.co', '.io', '.gov'];
-  let randomPronoun = Math.random(Math.floor()) 
 
 
-  console.log("Hello Rigo from the console!");
-};
+const myButton = document.getElementById('myButton');
+const myText = document.getElementById('myDomain');
+//Arrays
+let pronouns = ["the", "my", "his", "a", "our"];
+let adjectives = ["happy", "strange", "bright", "sleepy", "great"];
+let nouns = ["xylophone", "cactus", "hummingbird", "tulip", "waterfall"];
+let extensions = [".com", ".net", ".co", ".io", ".gov"];
+
+//Randomizers
+let randomPronoun = pronouns[Math.floor(Math.random() * pronouns.length)];
+let randomAdjective = adjectives[Math.floor(Math.random() * adjectives.length)];
+let randomNoun = nouns[Math.floor(Math.random() * nouns.length)];
+let randomExtension= extensions[Math.floor(Math.random() * extensions.length)];
+
+let newDomain = `${randomPronoun}${randomAdjective}${randomNoun}${randomExtension}`;
+
+myText.textContent = newDomain;
+
+
+myButton.addEventListener("click", function(){
+  let randomPronoun = pronouns[Math.floor(Math.random() * pronouns.length)];
+  let randomAdjective = adjectives[Math.floor(Math.random() * adjectives.length)];
+  let randomNoun = nouns[Math.floor(Math.random() * nouns.length)];
+  let randomExtension = extensions[Math.floor(Math.random() * extensions.length)];
+
+  let newDomain = `${randomPronoun}${randomAdjective}${randomNoun}${randomExtension}`;
+
+  myText.textContent = newDomain;
+
+});
