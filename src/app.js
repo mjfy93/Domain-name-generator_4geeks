f0ff0d32e134047c75b79ab94cf332bb45449475
mjfy93 <maria.jose.ferro@hotmail.com>
2025-08-15 11:57:59 -0500

    Correcciones finales

--- src/app.js
+++ src/app.js
@@ -15,15 +15,7 @@ let adjectives = ["happy", "strange", "bright", "sleepy", "great"];
 let nouns = ["xylophone", "cactus", "hummingbird", "tulip", "waterfall"];
 let extensions = [".com", ".net", ".co", ".io", ".gov"];
 
-//Randomizers
-let randomPronoun = pronouns[Math.floor(Math.random() * pronouns.length)];
-let randomAdjective = adjectives[Math.floor(Math.random() * adjectives.length)];
-let randomNoun = nouns[Math.floor(Math.random() * nouns.length)];
-let randomExtension= extensions[Math.floor(Math.random() * extensions.length)];
 
-let newDomain = `${randomPronoun}${randomAdjective}${randomNoun}${randomExtension}`;
-
-myText.textContent = newDomain;
 
 
 myButton.addEventListener("click", function(){
